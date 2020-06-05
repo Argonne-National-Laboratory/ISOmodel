bf53512c71163847764102f1662bb0114fc62a5e
XXX <dan.macumber@gmail.com>
2020-06-04 20:41:15 -0600

    Use conan to fetch dependencies

---
+++ IsoModel/src/ConanInstall.cmake
@@ -0,0 +1,46 @@
+# This file lists and installs the Conan packages needed
+
+set(CMAKE_CONAN_EXPECTED_HASH 773399d30bb924959b86883f95d64df6)
+set(CMAKE_CONAN_VERSION "v0.15")
+
+if(EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+  file(MD5 "${CMAKE_BINARY_DIR}/conan.cmake" CMAKE_CONAN_HASH)
+endif()
+
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake" OR NOT "${CMAKE_CONAN_HASH}" MATCHES "${CMAKE_CONAN_EXPECTED_HASH}")
+  message(STATUS "Downloading conan.cmake ${CMAKE_CONAN_VERSION} from https://github.com/conan-io/cmake-conan")
+  file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/${CMAKE_CONAN_VERSION}/conan.cmake"
+     "${CMAKE_BINARY_DIR}/conan.cmake")
+else()
+  message(STATUS "using existing conan.cmake")
+endif()
+
+include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+conan_check(VERSION 1.21.0 REQUIRED)
+
+message(STATUS "RUNNING CONAN")
+
+conan_add_remote(NAME bincrafters
+  URL https://api.bintray.com/conan/bincrafters/public-conan)
+
+list(APPEND CONAN_BUILD "outdated")
+
+# This will create the conanbuildinfo.cmake in the current binary dir, not the cmake_binary_dir
+conan_cmake_run(REQUIRES
+  boost/1.71.0
+  gtest/1.10.0
+ 
+  BASIC_SETUP CMAKE_TARGETS NO_OUTPUT_DIRS
+  OPTIONS ${CONAN_OPTIONS}
+  BUILD ${CONAN_BUILD}
+  # Passes `-u, --update`    to conan install: Check updates exist from upstream remotes
+  # That and build=outdated should ensure we track the right
+  UPDATE
+)
+
+set(CONAN_ALREADY_RUN TRUE)
+
+message(STATUS "DONE RUNNING CONAN")
+
+
